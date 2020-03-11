4f529f5be01856d47fe3e1f7f1d04e374d24f732
krishnarajan1997 <krishnarajan1997@gmail.com>
2020-03-11 23:48:23 +0530

    new base class

---
+++ src/test/java/org/test/select.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class select extends HelperClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		launchBrowser();
+		launchUrl("https://www.facebook.com/");
+		WebElement ty = findXpath("//a[text()='Careers']");
+	sleep(3000);
+	scrolDown(ty);
+}
+
+}
